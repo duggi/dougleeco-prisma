75b6a67b26b22ac554b06a498c85f7b7fe54de01
Doug Lee <douglee650@gmail.com>
2023-03-28 08:08:08 -0700

    Scaffold basic Next.js 13 with Prisma and Postgres

---
+++ data/items.ts
@@ -0,0 +1,16 @@
+export const items = [
+  {
+    id: "8a9020b2-363b-4a4f-ad26-d6d55b51bqes",
+    title: "Soccer Practice",
+    slug: "soccer-practice",
+    description: "Johnny's soccer at Woodside",
+    userId: "7122dadb-550e-4dce-96e2-c0a9355ba9be",
+  },
+  {
+    id: "2a3121b2-363b-4a4f-ad26-d6c35b41bade",
+    title: "Recycling Pickup",
+    slug: "recycling-pickup",
+    description: "Weekly trash and recycling",
+    userId: "7122dadb-550e-4dce-96e2-c0a9355ba9be",
+  },
+];
---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+// prisma/seed.ts
+
+import { PrismaClient } from '@prisma/client'
+import { items } from '../data/items'
+const prisma = new PrismaClient()
+
+async function main() {
+  //await prisma.user.create({
+  //  data: {
+  //    email: `douglee650@gmail.com`,
+  //    role: 'ADMIN',
+  //  },
+  //})
+
+  await prisma.item.createMany({
+    data: items,
+  })
+}
+
+main()
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
