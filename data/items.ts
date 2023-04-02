2b832b2b2723505a3323c05db5fe254133883d77
Doug Lee <douglee650@gmail.com>
2023-04-01 20:26:55 -0700

    Convert to Next.js 12
    * Remove Tailwind classes from templates
    * Add more items to data seeding

--- data/items.ts
+++ data/items.ts
@@ -1,16 +1,484 @@
 export const items = [
   {
-    id: "8a9020b2-363b-4a4f-ad26-d6d55b51bqes",
-    title: "Soccer Practice",
-    slug: "soccer-practice",
-    description: "Johnny's soccer at Woodside",
-    userId: "7122dadb-550e-4dce-96e2-c0a9355ba9be",
+    "title":"Soccer Practice",
+    "slug":"soccer-practice",
+    "description":"Johnny's soccer at Woodside",
+    "userId":"7122dadb-550e-4dce-96e2-c0a9355ba9be"
   },
   {
-    id: "2a3121b2-363b-4a4f-ad26-d6c35b41bade",
-    title: "Recycling Pickup",
-    slug: "recycling-pickup",
-    description: "Weekly trash and recycling",
-    userId: "7122dadb-550e-4dce-96e2-c0a9355ba9be",
+    "title":"Recycling Pickup",
+    "slug":"recycling-pickup",
+    "description":"Weekly trash and recycling",
+    "userId":"7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title":"Soccer Practice",
+    "slug":"soccer-practice",
+    "description":"Johnny's soccer at Woodside",
+    "userId":"7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title":"Recycling Pickup",
+    "slug":"recycling-pickup",
+    "description":"Weekly trash and recycling",
+    "userId":"7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title":"Basketball Practice",
+    "slug":"basketball-practice",
+    "description":"Joey's basketball at Elmwood",
+    "userId":"00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title":"Dentist Appointment",
+    "slug":"dentist-appointment",
+    "description":"Annual teeth cleaning and checkup",
+    "userId":"7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title":"Yoga Class",
+    "slug":"yoga-class",
+    "description":"Relaxing yoga session at the studio",
+    "userId":"00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title":"Grocery Shopping",
+    "slug":"grocery-shopping",
+    "description":"Weekly grocery trip to the store",
+    "userId":"7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title":"Doctor's Appointment",
+    "slug":"doctor-appointment",
+    "description":"Routine check-up with my GP",
+    "userId":"7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title":"Birthday Party",
+    "slug":"birthday-party",
+    "description":"Celebrating my friend's birthday at home",
+    "userId":"00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title":"Road Trip",
+    "slug":"road-trip",
+    "description":"Driving to the beach with friends",
+    "userId":"7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title":"Movie Night",
+    "slug":"movie-night",
+    "description":"Watching a comedy at the cinema",
+    "userId":"00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title":"Date Night",
+    "slug":"date-night",
+    "description":"Dinner and drinks with my partner",
+    "userId":"7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title":"Gym Session",
+    "slug":"gym-session",
+    "description":"Weightlifting and cardio at the gym",
+    "userId":"7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title":"Vacation",
+    "slug":"vacation",
+    "description":"Relaxing on the beach in Hawaii",
+    "userId":"00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title":"Grocery Shopping",
+    "slug":"grocery-shopping",
+    "description":"Buying food and supplies for the week",
+    "userId":"00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title":"Music Festival",
+    "slug":"music-festival",
+    "description":"Attending a weekend music festival",
+    "userId":"7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title":"Family Dinner",
+    "slug":"family-dinner",
+    "description":"Having dinner with my family",
+    "userId":"7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title":"Game Night",
+    "slug":"game-night",
+    "description":"Playing board games with friends",
+    "userId":"00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title":"Job Interview",
+    "slug":"job-interview",
+    "description":"Interviewing for a new job",
+    "userId":"7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title":"Volunteering",
+    "slug":"volunteering",
+    "description":"Helping out at a local charity",
+    "userId":"7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title":"Hiking",
+    "slug":"hiking",
+    "description":"Going on a hike in the mountains",
+    "userId":"00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title":"Movie Night",
+    "slug":"movie-night",
+    "description":"Watching a movie with friends",
+    "userId":"7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title":"Traveling",
+    "slug":"traveling",
+    "description":"Going on a trip to a new city",
+    "userId":"00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title":"Yoga Class",
+    "slug":"yoga-class",
+    "description":"Attending a yoga class at the gym",
+    "userId":"7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title":"Brunch",
+    "slug":"brunch",
+    "description":"Having brunch with friends",
+    "userId":"7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title":"Gardening",
+    "slug":"gardening",
+    "description":"Planting new flowers in the garden",
+    "userId":"00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title":"Reading",
+    "slug":"reading",
+    "description":"Reading a new book at home",
+    "userId":"7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title":"Date Night",
+    "slug":"date-night",
+    "description":"Going out on a date with my partner",
+    "userId":"7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+
+  {
+    "title": "Bike Ride",
+    "slug": "bike-ride",
+    "description": "Cycling through the city",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Cooking Class",
+    "slug": "cooking-class",
+    "description": "Learn how to cook a new recipe",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Movie Night",
+    "slug": "movie-night",
+    "description": "Watching a movie at home",
+    "userId": "7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title": "Basketball Game",
+    "slug": "basketball-game",
+    "description": "Playing basketball with friends",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Gardening",
+    "slug": "gardening",
+    "description": "Planting new flowers in the garden",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Yoga Class",
+    "slug": "yoga-class",
+    "description": "Relaxing yoga session",
+    "userId": "7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title": "Board Game Night",
+    "slug": "board-game-night",
+    "description": "Playing board games with friends",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Art Class",
+    "slug": "art-class",
+    "description": "Painting and drawing session",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Swimming",
+    "slug": "swimming",
+    "description": "Swimming laps at the pool",
+    "userId": "7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title": "Running",
+    "slug": "running",
+    "description": "Going for a run in the park",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Volunteering",
+    "slug": "volunteering",
+    "description": "Helping out at a local charity",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Gym Workout",
+    "slug": "gym-workout",
+    "description": "Weightlifting and cardio at the local gym",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Dog Walk",
+    "slug": "dog-walk",
+    "description": "Take the dog for a walk around the block",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Grocery Shopping",
+    "slug": "grocery-shopping",
+    "description": "Buy groceries for the week at the local supermarket",
+    "userId": "7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title": "Meeting with Manager",
+    "slug": "meeting-with-manager",
+    "description": "Discuss progress and plans with the manager",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Yoga Class",
+    "slug": "yoga-class",
+    "description": "Attend a yoga class at the local studio",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Movie Night",
+    "slug": "movie-night",
+    "description": "Watch a movie at home with friends",
+    "userId": "7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title": "Haircut Appointment",
+    "slug": "haircut-appointment",
+    "description": "Get a haircut at the local salon",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Dinner with Family",
+    "slug": "dinner-with-family",
+    "description": "Have dinner with family at a local restaurant",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Painting Class",
+    "slug": "painting-class",
+    "description": "Attend a painting class at a local art studio",
+    "userId": "7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title": "Doctor's Appointment",
+    "slug": "doctors-appointment",
+    "description": "Visit the doctor for a routine checkup",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Football Game",
+    "slug": "football-game",
+    "description": "Attend a football game with friends",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Dentist Appointment",
+    "slug": "dentist-appointment",
+    "description": "Get teeth cleaning and checkup",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Grocery Shopping",
+    "slug": "grocery-shopping",
+    "description": "Buy groceries for the week",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Birthday Party",
+    "slug": "birthday-party",
+    "description": "Celebrate Sarah's 10th birthday",
+    "userId": "7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title": "Book Club Meeting",
+    "slug": "book-club-meeting",
+    "description": "Discuss the latest book we read",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Gym Workout",
+    "slug": "gym-workout",
+    "description": "Weightlifting and cardio",
+    "userId": "7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title": "Piano Lesson",
+    "slug": "piano-lesson",
+    "description": "Learn a new song",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Bike Ride",
+    "slug": "bike-ride",
+    "description": "Go on a scenic bike ride",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+
+  {
+    "title": "Grocery Shopping",
+    "slug": "grocery-shopping",
+    "description": "Buy groceries for the week",
+    "userId": "7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title": "Yoga Class",
+    "slug": "yoga-class",
+    "description": "Morning yoga session at the park",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Movie Night",
+    "slug": "movie-night",
+    "description": "Watch a movie with friends",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Dinner Party",
+    "slug": "dinner-party",
+    "description": "Host a dinner party for friends",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Book Club Meeting",
+    "slug": "book-club-meeting",
+    "description": "Discuss the latest book with the book club",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Hiking Trip",
+    "slug": "hiking-trip",
+    "description": "Go for a hike at the nearby park",
+    "userId": "7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title": "Painting Class",
+    "slug": "painting-class",
+    "description": "Attend a painting class with friends",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "BBQ Party",
+    "slug": "bbq-party",
+    "description": "Host a BBQ party for family and friends",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Gardening",
+    "slug": "gardening",
+    "description": "Plant new flowers in the garden",
+    "userId": "7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title": "Basketball Game",
+    "slug": "basketball-game",
+    "description": "Play basketball with friends",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Volunteering",
+    "slug": "volunteering",
+    "description": "Volunteer at the local food bank",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Weekend Hike",
+    "slug": "weekend-hike",
+    "description": "Explore the local trails",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Movie Night",
+    "slug": "movie-night",
+    "description": "Watch a new release at the theater",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Date Night",
+    "slug": "date-night",
+    "description": "Try out a new restaurant",
+    "userId": "7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title": "Family Game Night",
+    "slug": "family-game-night",
+    "description": "Play board games with the kids",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Volunteer at Local Shelter",
+    "slug": "volunteer-at-local-shelter",
+    "description": "Walk dogs and play with cats",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Bike Ride",
+    "slug": "bike-ride",
+    "description": "Go for a leisurely ride through the park",
+    "userId": "7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title": "Picnic in the Park",
+    "slug": "picnic-in-the-park",
+    "description": "Pack a lunch and enjoy the sunshine",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Art Museum Visit",
+    "slug": "art-museum-visit",
+    "description": "View the latest exhibit at the local museum",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
+  },
+  {
+    "title": "Learn a New Language",
+    "slug": "learn-a-new-language",
+    "description": "Sign up for a beginner's course",
+    "userId": "7122dadb-550e-4dce-96e2-c0a9355ba9be"
+  },
+  {
+    "title": "Beach Day",
+    "slug": "beach-day",
+    "description": "Soak up the sun and swim in the ocean",
+    "userId": "00469aaa-1168-4ab1-bf91-45d9e02d1a0a"
+  },
+  {
+    "title": "Karaoke Night",
+    "slug": "karaoke-night",
+    "description": "Sing your heart out at the local bar",
+    "userId": "7a1a56f9-937d-4e70-ac0f-3efd8465ab9e"
   },
 ];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -12,6 +12,22 @@ async function main() {
   //  },
   //})
 
+  //await prisma.user.create({
+  //  data: {
+  //    email: `someuser123@gmail.com`,
+  //    role: 'USER',
+  //    name: 'Some User',
+  //  },
+  //})
+
+  //await prisma.user.create({
+  //  data: {
+  //    email: `anotheruser@yahoo.com`,
+  //    role: 'USER',
+  //    name: 'Another User',
+  //  },
+  //})
+
   await prisma.item.createMany({
     data: items,
   })
