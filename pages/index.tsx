b971a980898b0897be3127476ba6f078ed0028e4
Doug Lee <douglee650@gmail.com>
2023-04-02 16:47:34 -0700

    Add pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,39 @@
 import Head from 'next/head';
 //import { DougLeeCo } from '../components/DougLeeCo';
 //import { items } from '../data/items';
-import { gql, useQuery } from '@apollo/client'
-import type { Item } from '@prisma/client'
+import { gql, useQuery, useLazyQuery, useMutation } from '@apollo/client';
+import type { Item } from '@prisma/client';
 
 const AllItemsQuery = gql`
-  query {
-    items {
-      id
-      title
-      slug
-      description
+  query allItemsQuery($first: Int, $after: ID) {
+    items(first: $first, after: $after) {
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+      edges {
+        cursor
+        node {
+          id
+          uuid
+          title
+          slug
+          description
+        }
+      }
     }
   }
 `
 
-
-
 export default function Home() {
-  const { data, loading, error } = useQuery(AllItemsQuery)
+  const { data, loading, error, fetchMore } = useQuery(AllItemsQuery, {
+    variables: { first: 30 },
+  });
 
   if (loading) return <p>Loading...</p>
-  if (error) return <p>Oh no... {error.message}</p>
+  if (error) return <p>Error: {error.message}</p>
+
+  const {endCursor, hasNextPage } = data.items.pageInfo;
 
   return (
     <div>
@@ -32,15 +44,43 @@ export default function Home() {
 
       <div className="">
         <ol className="">
-          {data.items.map((item: Item) => (
+          {data?.items.edges.map(({ node }: { node: Item }) => (
             <li>
-              id={item.id} |
-              slug={item.slug} |
-              title={item.title} |
-              description={item.description}
+              id={node.id} |
+              uuid={node.uuid} |
+              slug={node.slug} |
+              title={node.title} |
+              description={node.description}
             </li>
           ))}
         </ol>
+
+
+        {hasNextPage ? (
+          <button
+            className=""
+            onClick={() => {
+              fetchMore({
+                variables: { after: endCursor },
+                updateQuery: (prevResult, { fetchMoreResult }) => {
+                  fetchMoreResult.items.edges = [
+                    ...prevResult.items.edges,
+                    ...fetchMoreResult.items.edges,
+                  ];
+                  return fetchMoreResult;
+                },
+              });
+            }}
+          >
+            more
+          </button>
+        ) : (
+          <p className="">
+            End of results{" "}
+          </p>
+        )}
+
+
       </div>
     </div>
   );
