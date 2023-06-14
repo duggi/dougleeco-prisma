00ce3fcb24d0c6139059e5db57452d28d96e7e78
Doug Lee <douglee650@gmail.com>
2023-06-14 00:58:47 -0700

    Rename path references to lowercase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "/styles/dlco-admin.css"
 import { UserProvider } from '@auth0/nextjs-auth0/client'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import { ApolloProvider } from '@apollo/client'
 import apolloClient from '../lib/apollo'
 import type { AppProps } from 'next/app'
